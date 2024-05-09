74a8c28d00eec63a9573d4ee2526043b4f6675ff
Leshchev Artem <usethepowerluke@gmail.com>
2024-05-09 14:08:47 +0200

    Add Kubernetes Insights (#7)
    Co-authored-by: Artem Leshchev <artem.leshchev@dxc.com>

---
+++ src/Bss.Platform.Kubernetes/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.ApplicationInsights.DependencyCollector;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bss.Platform.Kubernetes;
+
+public static class DependencyInjection
+{
+    private const string SqlHealthCheck = "SQLCheck";
+
+    public static IServiceCollection AddPlatformKubernetesInsights(this IServiceCollection services, IConfiguration configuration) =>
+        services
+            .AddApplicationInsightsTelemetry(configuration)
+            .ConfigureTelemetryModule<DependencyTrackingTelemetryModule>((x, _) => { x.EnableSqlCommandTextInstrumentation = true; })
+            .AddApplicationInsightsKubernetesEnricher();
+
+    public static IServiceCollection AddPlatformKubernetesHealthChecks(this IServiceCollection services, string connectionString)
+    {
+        services
+            .AddHealthChecks()
+            .AddSqlServer(connectionString, name: SqlHealthCheck);
+
+        return services;
+    }
+
+    public static IApplicationBuilder UsePlatformKubernetesHealthChecks(this IApplicationBuilder app) =>
+        app
+            .UseHealthChecks("/health/live", new HealthCheckOptions { Predicate = _ => false })
+            .UseHealthChecks("/health/ready", new HealthCheckOptions { Predicate = x => x.Name == SqlHealthCheck });
+}
