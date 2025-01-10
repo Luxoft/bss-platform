c672d5a67401af3393dfcf0565b993c4fa83bc0d
Ivan Gubanov <75603957+ivan-luxoft@users.noreply.github.com>
2025-01-11 01:56:09 +0200

    Add telemetry initializer (fill user id, role name; filter successful dependency / health checks) (#22)
    * update packages (exclude rabbit/cap), increment .net version, skip some vulnerability errors (package dependency)
    
    * add platform initialize, successful dependency filter and options to configure it
    
    * up .net version in workflow files
    
    * update readme
    
    * split initializer/rename to clarify logic for skipping health check requests

---
+++ src/Bss.Platform.Kubernetes/KubernetesInsightsOptions.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+
+namespace Bss.Platform.Kubernetes;
+
+public record KubernetesInsightsOptions
+{
+    public bool SkipSuccessfulDependency { get; set; } = false;
+
+    /// <summary>
+    /// Skip provided by this package health check probes ("/health/live", "/health/ready")
+    /// </summary>
+    public bool SkipDefaultHealthChecks { get; set; } = true;
+
+    /// <summary>
+    /// Register case-insensitive http path to skip from telemetry
+    /// <example> ["/DoHealthCheck"]</example>
+    /// </summary>
+    public string[] AdditionalHealthCheckPathToSkip { get; set; } = [];
+
+    /// <summary>
+    /// Fill AuthenticatedUserId (from HttpContext.User.Identity.Name)
+    /// </summary>
+    public bool SetAuthenticatedUserFromHttpContext { get; set; } = true;
+
+    /// <summary>
+    /// Used to set role name on AppInsight, by default it is assembly name
+    /// </summary>
+    public string RoleName { get; set; } = Assembly.GetEntryAssembly()?.GetName().Name ?? string.Empty;
+}
---
+++ src/Bss.Platform.Kubernetes/Services/TelemetryDataEnrichInitializer.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+
+using Microsoft.ApplicationInsights.AspNetCore.TelemetryInitializers;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+
+namespace Bss.Platform.Kubernetes.Services;
+
+internal class TelemetryDataEnrichInitializer(IHttpContextAccessor accessor, IOptions<KubernetesInsightsOptions> options) : TelemetryInitializerBase(accessor)
+{
+    protected override void OnInitializeTelemetry(HttpContext platformContext, RequestTelemetry requestTelemetry, ITelemetry telemetry)
+    {
+        if (options.Value.SetAuthenticatedUserFromHttpContext
+            && string.IsNullOrEmpty(telemetry.Context.User.AuthenticatedUserId)
+            && platformContext.User.Identity?.Name is { Length: > 0 } username)
+        {
+            telemetry.Context.User.AuthenticatedUserId = username;
+        }
+
+        if (string.IsNullOrEmpty(telemetry.Context.Cloud.RoleName))
+        {
+            telemetry.Context.Cloud.RoleName = options.Value.RoleName;
+        }
+    }
+}
