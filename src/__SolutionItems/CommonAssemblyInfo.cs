18aeb555aca582af2a8fa1f24b83d73649805c81
Andrey Komashko <ankomdr@yandex.ru>
2025-10-30 16:47:25 +0100

    Enhance debug experience (#30)
    * debug info embedding
    
    * ref to rep  + version
    
    ---------
    
    Co-authored-by: Andrey Komashko <akomashko@luxoft.com>

--- src/__SolutionItems/CommonAssemblyInfo.cs
+++ src/__SolutionItems/CommonAssemblyInfo.cs
@@ -1,12 +1,12 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 [assembly: AssemblyProduct("BSS Platform Services")]
 [assembly: AssemblyCompany("Luxoft")]
 [assembly: AssemblyCopyright("Copyright © Luxoft 2025")]
 
-[assembly: AssemblyVersion("1.5.10.0")]
-[assembly: AssemblyFileVersion("1.5.10.0")]
-[assembly: AssemblyInformationalVersion("1.5.10.0")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyInformationalVersion("1.6.0.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
