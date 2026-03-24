736f661bc93c99262310899552656164f937fb7d
Kirill Moroz <kirill.moroz96@gmail.com>
2026-03-24 13:21:16 +0200

    Update CommonAssemblyInfo.cs (#35)

--- src/__SolutionItems/CommonAssemblyInfo.cs
+++ src/__SolutionItems/CommonAssemblyInfo.cs
@@ -4,9 +4,9 @@ using System.Reflection;
 [assembly: AssemblyCompany("Luxoft")]
 [assembly: AssemblyCopyright("Copyright © Luxoft 2026")]
 
-[assembly: AssemblyVersion("1.6.2.0")]
-[assembly: AssemblyFileVersion("1.6.2.0")]
-[assembly: AssemblyInformationalVersion("1.6.2.0")]
+[assembly: AssemblyVersion("1.6.3.0")]
+[assembly: AssemblyFileVersion("1.6.3.0")]
+[assembly: AssemblyInformationalVersion("1.6.3.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
